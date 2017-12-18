2ef4507481d562f5e9d28a3ba02a6e1bf8143f5d
RyuaNerin <admin@ryuanerin.kr>
2017-12-19 05:07:26 +0900

    소스코드를 별개 폴더로 이동

---
+++ src/resource.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef __H__FIME_RESOURCES__
+#define __H__FIME_RESOURCES__
+
+#define IDI_ICON    1
+
+#define FIME_VERSION      0,0,0,13
+#define FIME_VERSION_STR "0.0.0.13"
+
+#endif
